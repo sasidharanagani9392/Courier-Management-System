88e838dac26d5eb4188dba61848647c5c5f7f4cd
sasidharanagani9392 <147024820+sasidharanagani9392@users.noreply.github.com>
2025-04-09 07:23:23 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,130 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+<!DOCTYPE html>
+<html lang="en">
+<?php session_start() ?>
+<?php 
+	if(!isset($_SESSION['login_id']))
+	    header('location:login.php');
+    include 'db_connect.php';
+    ob_start();
+  if(!isset($_SESSION['system'])){
+
+    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
+    foreach($system as $k => $v){
+      $_SESSION['system'][$k] = $v;
+    }
+  }
+  ob_end_flush();
+
+	include 'header.php' 
 ?>
-Something is wrong with the XAMPP installation :-(
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+  <?php include 'topbar.php' ?>
+  <?php include 'sidebar.php' ?>
+
+  
+  <div class="content-wrapper">
+  	 <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
+	    <div class="toast-body text-white">
+	    </div>
+	  </div>
+    <div id="toastsContainerTopRight" class="toasts-top-right fixed"></div>
+    
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+         
+
+        </div>
+            
+      </div>
+    </div>
+    
+
+    
+    <section class="content">
+      <div class="container-fluid">
+         <?php 
+            $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+            if(!file_exists($page.".php")){
+                include '404.html';
+            }else{
+            include $page.'.php';
+
+            }
+          ?>
+      </div>
+    </section>
+    
+    <div class="modal fade" id="confirm_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
+      </div>
+      <div class="modal-body">
+        <div id="delete_content"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal_right" role='dialog'>
+    <div class="modal-dialog modal-full-height  modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span class="fa fa-arrow-right"></span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="viewer_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+              <img src="" alt="">
+      </div>
+    </div>
+  </div>
+  </div>
+  
+
+  
+  <aside class="control-sidebar control-sidebar-dark">
+    
+  </aside>
+  
+
+
+</div>
+
+
+
+
+
+<?php include 'footer.php' ?>
+</body>
+</html>
